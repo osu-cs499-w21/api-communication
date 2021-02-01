5a496245ffe52601d7baa5a3e26a8071b48168af
Rob Hess <robwhess@gmail.com>
2021-02-01 15:01:38 -0800

    Add code from 2/1 lecture, part 1.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import fetch from 'isomorphic-unfetch';
 
 function Search({ query }) {
   const [ inputQuery, setInputQuery ] = useState(query || "");
+  const [ repos, setRepos ] = useState([]);
   const history = useHistory();
+
+  useEffect(() => {
+    async function fetchSearchResults() {
+      const res = await fetch(`https://api.github.com/search/repositories?q=${query}`);
+      const responseBody = await res.json();
+      setRepos(responseBody.items || []);
+      console.log("== repos:", responseBody.items);
+    }
+    fetchSearchResults();
+  }, [ query ]);
 
   return (
     <div>
@@ -15,6 +27,13 @@ function Search({ query }) {
         <button type="submit">Search</button>
       </form>
       <h2>Search query: {query}</h2>
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.id}>
+            <a href={repo.html_url}>{repo.full_name}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
